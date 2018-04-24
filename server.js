74ff1d30e6d284d8463ddd5dd5b821015febc90e
Katy Bowers <katyvine@gmail.com>
2018-04-24 12:42:14 -0700

    build out server

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const cors = require ('cors');
+const pg = require('pg');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(cors());
+
+app.get('/test', (req,res) => res.send('hello world'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
